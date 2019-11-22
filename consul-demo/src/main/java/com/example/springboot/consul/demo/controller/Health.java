c1d2a508371fb179c12f87492dc8deae386d2c3c
kvaghela1984 <kamlesh.vaghela@gmail.com>
2019-11-21 21:06:44 -0600

    clean

--- consul-demo/src/main/java/com/example/springboot/consul/demo/controller/Health.java
+++ consul-demo/src/main/java/com/example/springboot/consul/demo/controller/Health.java
@@ -1,6 +1,8 @@
 package com.example.springboot.consul.demo.controller;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class Health {
 
 	@Value("${my.property}")
-	String value;
+	String appProperty;
 	
+	@Value("${common.property}")
+	String commonProperty;
+
 	@Autowired
-    private DiscoveryClient discoveryClient;
+	private DiscoveryClient discoveryClient;
+
+	private Map<String, URI> map = new HashMap<>();
 
 	@RequestMapping("/health")
 	public String health() {
-		
-		Optional<URI> findFirst = discoveryClient.getInstances("consul-demo")
-        .stream()
-        .map(si -> si.getUri())
-        .findFirst();
-		
-		System.out.println(findFirst.get());
-		
-		System.out.println(value);
+		System.out.println(appProperty);
+		System.out.println(commonProperty);
 		return "Healthy";
+	}
+
+	@RequestMapping("/services")
+	public Map<String, URI> getAllServices() {
+		discoveryClient.getServices().forEach(s -> {
+			Optional<URI> uri = discoveryClient.getInstances(s).stream().map(si -> si.getUri()).findFirst();
+			map.put(s, uri == null ? null : uri.get());
+
+		});
+		return map;
 	}
 
 }
